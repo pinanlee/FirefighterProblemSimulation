ea8bae2042b307752eb905cdc621fa327061d8c2
deemp096 <pongjenren@gmail.com>
2023-08-08 10:25:48 +0800

    node_status_update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QMessageBox
 from PyQt5.QtGui import QPainter, QPen
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 import random 
 import json
 
@@ -9,12 +9,28 @@ NODE_NUMBER = 10
 NODE_POS = {}
     
 class Node(QPushButton):
-    def __init__(self, *args):
+    def __init__(self, q, b, h, *args):
         super().__init__(*args)
+        self.q = q
+        self.b = b
+        self.h = h
+        self.r = q*h
 
-class Deactivated_Node(Node):
-    def __init__(self, *args):
-        super().__init__(*args)
+class Burned_Node(Node):
+    def __init__(self, node, *args):
+        super().__init__(node.q, node.b, node.h, *args)
+
+class Burning_Node(Node):
+    def __init__(self, node, *args):
+        super().__init__(node.q, node.b, node.h, *args)
+
+class Processing_Node(Node):
+    def __init__(self, node, *args):
+        super().__init__(node.q, node.b, node.h, *args)
+
+class Processed_Node(Node):
+    def __init__(self, node, *args):
+        super().__init__(node.q, node.b, node.h, *args)
 
 class MyWidget(QWidget):
     def __init__(self, mode):
@@ -30,13 +46,62 @@ class MyWidget(QWidget):
         t_plus.setText('next time step')
         t_plus.move(10,850)
         t_plus.clicked.connect(self.next_time)
+
         self.t_text = QLabel(self)
         self.t_text.setText("time = " + str(self.time))
-        self.t_text.setFixedWidth(70)
+        self.t_text.setFixedWidth(120)
+        self.t_text.setStyleSheet("font: 18pt;")
         
+        self.mytimer = QTimer(self)
+        self.mytimer.timeout.connect(self.next_time)
+        self.mytimer.start(200)
+
+
     def next_time(self):
         self.time += 1
+        if self.time > self.T[-1]:
+            self.mytimer.stop()
+            return
         self.t_text.setText("time = " + str(self.time))
+
+        for i in self.node_btn:
+            if type(self.node_btn[i]) is Burning_Node:
+                self.node_btn[i].h -= 1
+                if self.node_btn[i].h <=0:
+                    temp = Burned_Node(self.node_btn[i], self)
+                    temp.move(self.node_btn[i].x(), self.node_btn[i].y())
+                    temp.setText(self.node_btn[i].text())
+                    temp.show()
+                    self.node_btn[i].deleteLater
+                    self.node_btn[i] = temp
+            elif type(self.node_btn[i]) is Processing_Node:
+                self.node_btn[i].r -= self.p[1]
+                if self.node_btn[i].r <= 0:
+                    temp = Processed_Node(self.node_btn[i], self)
+                    temp.move(self.node_btn[i].x(), self.node_btn[i].y())
+                    temp.setText(self.node_btn[i].text())
+                    temp.show()
+                    self.node_btn[i].deleteLater
+                    self.node_btn[i] = temp
+
+
+        for i in range(len(self.node_btn)):
+            if self.u[(i+1,self.time)] == 1:
+                temp = Burning_Node(self.node_btn[i+1], self)
+                temp.move(self.node_btn[i+1].x(), self.node_btn[i+1].y())
+                temp.setText(self.node_btn[i+1].text())
+                temp.show()
+                self.node_btn[i+1].deleteLater()
+                self.node_btn[i+1] = temp
+            for k in self.K:
+                if self.u_bar[(i+1, k, self.time)] == 1:
+                    temp = Processing_Node(self.node_btn[i+1], self)
+                    temp.move(self.node_btn[i+1].x(), self.node_btn[i+1].y())
+                    temp.setText(self.node_btn[i+1].text())
+                    temp.show()
+                    self.node_btn[i+1].deleteLater()
+                    self.node_btn[i+1] = temp
+
         
 
     def load_from_json(self):
@@ -44,23 +109,57 @@ class MyWidget(QWidget):
         with open("data.json", 'r') as file:
             data = file.read()
         data = json.loads(data)
-        self.N = data['N']
-        self.N_D = data['N_D']
-        self.N_F = data['N_F']
+        self.N = set(data['N'])
+        self.N_D = set(data['N_D'])
+        self.N_F = set(data['N_F'])
+        self.K = data['K']
         self.A_p = data['A_p']
         self.A_f = data['A_f']
         self.A_p = list([tuple(map(int, i[1:-1].split(','))) for i in data['A_p']])
         self.A_f = list([tuple(map(int, i[1:-1].split(','))) for i in data['A_f']])
-        
-        
+        self.x = dict([(tuple(map(int, i[1:-1].split(','))),data['x'][i]) for i in data['x']])
+        self.v = dict([(tuple(map(int, i[1:-1].split(','))),data['v'][i]) for i in data['v']])
+        self.v_bar = dict([(tuple(map(int, i[1:-1].split(','))),data['v_bar'][i]) for i in data['v_bar']])
+        self.u = dict([(tuple(map(int, i[1:-1].split(','))),data['u'][i]) for i in data['u']])
+        self.u_bar = dict([(tuple(map(int, i[1:-1].split(','))),data['u_bar'][i]) for i in data['u_bar']])
+        self.T = data['T']
+        self.q = dict([(int(i), data['q'][i]) for i in data['q']])
+        self.b = dict([(int(i), data['b'][i]) for i in data['b']])
+        # self.p = data['p']
+        self.p = {1:3}
+        self.h = dict([(int(i), data['h'][i]) for i in data['h']])
         
         self.NODE_POS = data['NODE_POS']
-        node_btn = []
-        for i in self.N:
-            node_btn.append(Node(self))
-            node_btn[i-1].move(self.NODE_POS[str(i)][0], self.NODE_POS[str(i)][1])
-            node_btn[i-1].setText(str(i))
+        # self.node_btn = []
+        # for i in self.N:
+        #     self.node_btn.append(Node(self.q[i], self.b[i], self.h[i],self))
+        #     self.node_btn[i-1].move(self.NODE_POS[str(i)][0], self.NODE_POS[str(i)][1])
+        #     self.node_btn[i-1].setText(str(i))
+        # for i in self.N_F:
+        #     temp = Burning_Node(self)
+        #     temp.move(self.node_btn[i-1].x(), self.node_btn[i-1].y())
+        #     temp.setText(self.node_btn[i-1].text())
+        #     self.node_btn[i-1].deleteLater
+        #     self.node_btn[i-1] = temp
 
+        self.node_btn = {}
+        for i in self.N-self.N_D:
+            print(i)
+            self.node_btn[i] =Node(self.q[i], self.b[i], self.h[i],self)
+            self.node_btn[i].move(self.NODE_POS[str(i)][0], self.NODE_POS[str(i)][1])
+            self.node_btn[i].setText(str(i))
+
+        for i in self.N_D:
+            self.node_btn[i] = Node(0, 0, 0, self)
+            self.node_btn[i].move(self.NODE_POS[str(i)][0], self.NODE_POS[str(i)][1])
+            self.node_btn[i].setText(str(i))
+
+        for i in self.N_F:
+            temp = Burning_Node(self.node_btn[i], self)
+            temp.move(self.node_btn[i].x(), self.node_btn[i].y())
+            temp.setText(self.node_btn[i].text())
+            self.node_btn[i].deleteLater
+            self.node_btn[i] = temp
 
     def random_generate(self):
         node_btn = []
