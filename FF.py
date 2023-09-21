9f46be406ca9217d40d992197ebd18f7da37daf4
pinanlee <h34091089@gs.ncku.edu.tw>
2023-09-21 23:40:14 +0800

    fix bug

--- FF.py
+++ FF.py
@@ -23,7 +23,7 @@ class FireFighter(QLabel):
         self.__process = False #是否在澆水
         self.__ready = False #是否已準備好
         self.rate_extinguish = 2 #澆水速率
-        self.move_man = 20 #移動速率
+        self.move_man = 10 #移動速率
         self.destNode = None #下一個目的
         self.curMovingArc : dict = None
         self.pathProgress = 0
